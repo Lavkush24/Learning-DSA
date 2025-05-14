57aed0a6cbb055afdd4e75814918da50b1e00353
Lavkush kushwaha <college.studying23@gmail.com>
2025-05-14 23:28:00 +0530

    Search operation implementation

--- trees/BST.cpp
+++ trees/BST.cpp
@@ -3,6 +3,35 @@ using namespace std;
 #include "Tree.h"
 
 
+bool RsearchTree(node* t,int key) {
+    if(t == NULL) {
+        return 0;
+    }
+    else if(key == t->data) {
+        return 1;
+    }
+    else if(key < t->data) {
+        return RsearchTree(t->Lchild,key);
+    }
+    else {
+        return RsearchTree(t->Rchild,key);
+    }
+}
+
+bool iterativeSearch(node *t, int key) {
+    while(t != NULL) {
+        if(key == t->data) {
+            return 1;
+        }
+        else if(key < t->data) {
+            t = t->Lchild;
+        }
+        else {
+            t = t->Rchild;
+        }
+    }
+    return 0;
+}
 
 
 
@@ -10,5 +39,7 @@ int main() {
     Tree t;
     t.create();
     t.inorderTraversal(t.getRoot());
+    cout<<RsearchTree(t.getRoot(),12)<<endl;
+    cout<<iterativeSearch(t.getRoot(),34)<<endl;
     return 0;
 }
