287dd865cc9fc32d45b6f9303cb1754193b11602
Lavkush24 <college.studying23@gmail.com>
2025-10-01 21:49:07 +0530

    quick sort

--- sorting_techniques/sort.cpp
+++ sorting_techniques/sort.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#include<climits>
 
 
 void bubble_sort(int arr[],int s) {
@@ -30,6 +31,52 @@ void insertion_sort(int arr[],int s) {
     }
 }
 
+void selection_sort(int arr[],int s) {
+    for(int i=0; i<s-1; i++) {
+        int k ,j;
+        k = j = i; 
+        while(j < s) {
+            if(arr[j] < arr[k]) {
+                k = j;
+            }
+            j++;
+        }
+        int temp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = temp;
+    }
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a  = *b;
+    *b = temp;
+}
+
+int partition(int arr[],int l, int h) {
+    int pivot = arr[l];
+    int i=l, j=h;
+
+    do {
+        do { i++; } while(arr[i] <= pivot);
+        do { j--; } while(arr[j] > pivot);
+
+        if(i < j) swap(&arr[i], &arr[j]);
+    }while(i < j);
+
+    swap(&arr[j],&arr[l]);
+    return j;
+}
+
+void quick_sort(int arr[], int l, int h) {
+    if(l<h) {
+        int p = partition(arr,l,h);
+        quick_sort(arr,l,p);
+        quick_sort(arr,p+1,h);
+    }
+}
+
+
 void print(int arr[],int s) {
     for(int i =0; i<s-1; i++) {
         cout<<arr[i]<<" ";
@@ -41,7 +88,7 @@ int main() {
     int arr[] = {8,5,7,3,2};
     int s = 5;
 
-    insertion_sort(arr,s);
+    quick_sort(arr,0,5);
 
     print(arr,s);
     return 0;
