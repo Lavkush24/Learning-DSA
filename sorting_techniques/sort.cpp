dfdae477d86a5f21bfdf7d58b4f7523738665311
Lavkush24 <college.studying23@gmail.com>
2025-10-02 12:12:04 +0530

    merge sort, count sort

--- sorting_techniques/sort.cpp
+++ sorting_techniques/sort.cpp
@@ -77,6 +77,119 @@ void quick_sort(int arr[], int l, int h) {
 }
 
 
+int* merging_list(int l1[],int l2[],int n,int m) {
+    int *l3 = new int[n+m];
+    int i=0,j=0,k=0;
+    while(i < n && j < m) {
+        if(l1[i] <= l2[j]) {
+            l3[k] = l1[i];
+            i++,k++;
+        }
+        else if(l1[i] > l2[j]) {
+            l3[k] = l2[j];
+            j++,k++;
+        }
+    }
+
+    for( ; i<n; i++) { l3[k++] = l1[i]; }
+    for( ; j<m; j++) { l3[k++] = l2[j]; }
+
+    return l3;
+}
+
+void merge_single(int l[],int s,int mid,int h) {
+    int i,j,k;
+    i=s,j=mid+1,k=0;
+    int *l2 = new int[h+1];
+
+    while(i <= mid && j <= h) {
+        if(l[i] < l[j]) {
+            l2[k++] = l[i++];
+        }else {
+            l2[k++] = l[j++];
+        }
+    }
+
+    for(;i<=mid;i++) { l2[k++] = l[i];}
+    for(;j<=h;j++) { l2[k++] = l[j];}
+
+    k = 0 ;
+    for(i=s; i<=h; i++) {
+        l[i] = l2[k++];
+    }
+
+    delete[] l2 ;
+}
+
+
+void iterative_merge_sort(int arr[],int n) {
+    int p,i,l,mid,h;
+
+    for(p=2; p<=n; p=p*2) {
+        l = 0, h = l+p-1;
+        mid = (l+h)/2;
+        while(h < n) {
+            merge_single(arr,l,mid,h);
+            if(n % p > 0) {
+                merge_single(arr,0,h,n-1);
+            }
+            l = h+1;
+            h = l+p-1;
+            mid = (l+h)/2;
+        }
+
+    }
+}
+
+
+void recursive_merge_sort(int arr[],int l,int h) {
+    if(l<h) {
+        int mid = (l+h)/2;
+        recursive_merge_sort(arr,l,mid);
+        recursive_merge_sort(arr,mid+1,h);
+        merge_single(arr,l,mid,h);
+    }
+
+}
+
+int max(int arr[],int s) {
+    int c = INT_MIN;
+    for(int i=0;i<s; i++) {
+        if(c < arr[i]) {
+            c = arr[i];
+        }
+    }
+    return c;
+}
+
+void count_sort(int arr[],int s) {
+    int m = max(arr,s);
+
+    int *temp = new int[m+1];
+
+    for(int i=0; i<m+1; i++) {
+        temp[i] = 0;
+    }
+
+    for(int i=0; i<s; i++) {
+        temp[arr[i]]++;
+    }
+
+    int i = 0;
+    for(int j=0; j<=m; j++) {
+        while(temp[j] > 0) {
+            arr[i] = j;
+            i++,temp[j]--;
+        }
+    }
+
+    delete[] temp;
+}
+
+
+
+
+
 void print(int arr[],int s) {
     for(int i =0; i<s-1; i++) {
         cout<<arr[i]<<" ";
@@ -85,12 +198,12 @@ void print(int arr[],int s) {
 }
 
 int main() {
-    int arr[] = {8,5,7,3,2};
-    int s = 5;
-
-    quick_sort(arr,0,5);
-
-    print(arr,s);
+    
+    int l[] = {4,6,7,81,2,3,4,22};
+    int t = sizeof(l)/sizeof(l[0]);;
+    
+    count_sort(l,t);
+    print(l,t);
     return 0;
 }
 
